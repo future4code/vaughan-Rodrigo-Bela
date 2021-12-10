a83eddfa74d164098c981b653792191e92d98896
Rodrigo Brito <rodrigo.lab@yahoo.com>
2021-12-10 20:11:47 -0300

    Adicionado arquivo do projeto-10-12

---
+++ Modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,37 @@
+
+
+
+console.log ("Boas vindas ao jogo de Blackjack!")
+if(confirm("Quer iniciar uma nova rodada?")) {
+	const nomeDoJogador = prompt("Lega, qual é o seu nome?")
+    let carta1 = comprarCarta();
+   let carta2 = comprarCarta();
+   let carta3pc = comprarCarta();
+   let carta4pc = comprarCarta()
+   let pontoDoUsuario = carta1Usuario.valor + carta2Usuario.valor
+   let pontoDopc = carta3pc.valor + carta4pc.valor
+
+
+
+
+   
+   const comparar = () => { 
+      
+
+
+
+   }
+
+
+
+   console.log(nomeDoJogador + carta.texto)                    // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+   console.log("Computador" + carta.valor)                                            // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+   
+   
+   console.log ("")
+   
+   
+   // o que fazer se o usuário clicar "ok"
+} else { alert("O jogo terminou")
+	// o que fazer se o usuário clicar "cancelar"
+}

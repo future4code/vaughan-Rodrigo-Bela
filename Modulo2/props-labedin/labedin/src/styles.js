2232d12fa32c83c8376c5e9d0508ae63862911ba
Rodrigo Brito <rodrigo.lab@yahoo.com>
2022-01-11 21:38:59 -0300

    Arquivos da Aula de Styled-Components

---
+++ Modulo2/props-labedin/labedin/src/styles.js
@@ -0,0 +1,163 @@
+// import styled from "styled-components";
+
+
+//    const stiloCardGrande = styled.bigcard-container`
+
+//         display: flex;
+//         align-items: center;
+//         border: 1px solid black;
+//         padding: 20px 10px;
+//         margin-bottom: 10px;
+//         height: 200px;
+//     `
+    
+//     const stiloCardGrande1 = styled.bigcard-container > img`
+//         width: 70px;
+//         margin-right: 10px;
+//         border-radius: 50%;
+//     `
+    
+//     stiloCardGrande2 = styled.bigcard-container > h4`
+//         margin-bottom: 15px;
+//     `
+    
+//     stiloCardGrande3 = styled.bigcard-container > div`
+//         display: flex;
+//         flex-direction: column;
+//         justify-items: flex-start;
+//     `
+    
+
+    
+
+
+
+
+// .bigcard-container {
+//     display: flex;
+//     align-items: center;
+//     border: 1px solid black;
+//     padding: 20px 10px;
+//     margin-bottom: 10px;
+//     height: 200px;
+// }
+
+// .bigcard-container > img {
+//     width: 70px;
+//     margin-right: 10px;
+//     border-radius: 50%;
+// }
+
+// .bigcard-container h4 {
+//     margin-bottom: 15px;
+// }
+
+// .bigcard-container > div {
+//     display: flex;
+//     flex-direction: column;
+//     justify-items: flex-start;
+// }
+
+
+
+
+
+// .smallcard-container {
+//     display: flex;
+//     align-items: center;
+//     border: 1px solid black;
+//     padding: 20px 10px;
+//     margin-bottom: 10px;
+//     height: 140px;
+// }
+
+// .smallcard-container > img {
+//     width: 70px;
+//     margin-right: 10px;
+//     border-radius: 50%;
+// }
+
+// .smallcard-container h4 {
+//     margin-bottom: 15px;
+// }
+
+// .smallcard-container > div {
+//     display: flex;
+//     flex-direction: column;
+//     justify-items: flex-start;
+// }
+
+
+
+
+
+
+// .image-button-container {
+//     display: flex;
+//     align-items: center;
+//     border: 1px solid black;
+//     border-radius: 50px;
+//     width: 200px;
+//     padding: 15px 30px;
+//     margin: 10px auto;
+// }
+
+// .image-button-container > img {
+//     width: 30px;
+//     margin-right: 10px;
+// }
+
+
+
+
+
+
+// * {
+//     padding: 0;
+//     margin: 0;
+//     box-sizing: border-box;
+//   }
+  
+//   .App {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     margin-top: 30px;
+//   }
+  
+//   .page-section-container {
+//     width: 40vw;
+//     margin: 10px 0;
+//   }
+  
+//   .page-section-container > h3 {
+//     text-align: center;
+//     margin-bottom: 20px;
+//   }
+  
+//   h2 {
+//     display: flex;
+//     justify-content: center;
+//     margin-bottom: 20px;
+//   }
+
+
+
+
+
+
+
+// body {
+//     margin: 0;
+//     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+//       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+//       sans-serif;
+//     -webkit-font-smoothing: antialiased;
+//     -moz-osx-font-smoothing: grayscale;
+//   }
+  
+//   code {
+//     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+//       monospace;
+//   }
+  
